63605e4313e67571f412cf7e2a9b3f2c528685d3
Paweł Waleczek <pawel@thisismyasterisk.org>
2013-09-23 14:21:22 +0200

    update to support arrays

--- test/spec.all.js
+++ test/spec.all.js
@@ -21,16 +21,14 @@ describe('Redis Pub/Sub', function () {
       data.should.be.a('object')
       data.key.should.eql('test message')
 
-      sub() // unsubscribe
-
-      done()
+      sub(done()) // unsubscribe
     })
 
     PubSub.emit(channelName, messageObject)
 
   })
 
-  it('should be able to use queries to filter messages', function (done) {
+  it('should use queries to filter messages', function (done) {
 
     var channelName = 'spec.test'
       , channelPattern = 'spec.*'
@@ -40,9 +38,10 @@ describe('Redis Pub/Sub', function () {
           , { age: 21, key: 'none'}
         ]
     var sub = PubSub.on({ $has: ['key', 'age'], age: {$gt: 18}}, {}, function (data) {
-      sub()
+      data.should.be.a('object')
+      data.key.should.eql('none')
 
-      done()
+      sub(done())
     })
     // send multiple messages
     messageObject.forEach(function (message) {
@@ -50,11 +49,53 @@ describe('Redis Pub/Sub', function () {
     })
   })
 
-  it('should be able to remove all filter queue objects, aka unsubscribe', function (done) {
+  it('should use queries to filter arry messages', function (done) {
+
+    var channelName = 'spec.test'
+      , channelPattern = 'spec.*'
+      , messageObject = [
+            { key: 'test message' }
+          , { age: 10, key: 'other message' }
+          , { age: 21, key: 'none'}
+        ]
+    var sub = PubSub.on({ $has: ['key', 'age'], age: {$lt: 18}}, {}, function (data) {
+      data.should.be.instanceof(Array)
+      data[0].key.should.eql('other message')
+
+      sub(done())
+    })
+    PubSub.emit(channelName, messageObject)
+
+  })
+
+  it('should unsubscribe from a query', function (done) {
+    var sub = PubSub.on({}, {}, function (data) {
+      // no need to process
+    })
+
+    PubSub.off(sub, done)
+  })
+
+  it('should unsubscribe from multiple queries', function (done) {
+    var sub1 = PubSub.on({}, {}, function (data) {
+      // no need to process
+    })
+    var sub2 = PubSub.on({}, {}, function (data) {
+      // no need to process
+    })
+
+    var sub3 = PubSub.on({}, {}, function (data) {
+      // no need to process
+    })
+
+    PubSub.off([sub1, sub2, sub3], done)
+  })
+
+  it('should remove all filter queue objects, aka unsubscribe', function (done) {
     PubSub.clear(done)
   })
 
-  it('should be able to clean up', function (done) {
+  it('should clean up', function (done) {
     PubSub.cleanUp(done)
   })
 
